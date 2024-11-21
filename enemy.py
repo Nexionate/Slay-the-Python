fbd10c2a2f29e86577083e17d7ff1563480e92ff
Ethan O'Connor <ethan@northnode.ca>
2024-11-20 17:10:38 -0800

    Update enemy values, was setting them to 1HP for testing purposes

--- enemy.py
+++ enemy.py
@@ -1,4 +1,4 @@
 enemies_easy = (
-    {"name": "mugger", "current HP": 6, "max HP": 36, "current block": 0, "attack": [{"damage": 7, "block": 0}, {"damage": 13, "block": 0}, {"damage": 5, "block": 10}, {"damage": 0, "block": 10}]},
+    {"name": "mugger", "current HP": 1, "max HP": 36, "current block": 0, "attack": [{"damage": 7, "block": 0}, {"damage": 13, "block": 0}, {"damage": 5, "block": 10}, {"damage": 0, "block": 10}]},
     {"name": "slime", "current HP": 1, "max HP": 21, "current block": 0, "attack": [{"damage": 4, "block": 0}, {"damage": 6, "block": 6}, {"damage": 5, "block": 0}]}
     )
