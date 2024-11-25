abf77962cc7833806f5e183237a280db8e0c4542
Ethan O'Connor <ethan@northnode.ca>
2024-11-24 20:17:14 -0800

    Added harder enemies for elite battles

--- enemy.py
+++ enemy.py
@@ -1,4 +1,10 @@
 enemies_easy = (
-    {"name": "mugger", "current HP": 1, "max HP": 36, "current block": 0, "attack": [{"damage": 7, "block": 0}, {"damage": 13, "block": 0}, {"damage": 5, "block": 10}, {"damage": 0, "block": 10}]},
-    {"name": "slime", "current HP": 1, "max HP": 21, "current block": 0, "attack": [{"damage": 4, "block": 0}, {"damage": 6, "block": 6}, {"damage": 5, "block": 0}]}
+    {"name": "mugger", "current HP": 1, "max HP": 36, "current block": 0, "attack pattern": False, "attack": [{"damage": 7, "block": 0}, {"damage": 11, "block": 0}, {"damage": 5, "block": 10}, {"damage": 0, "block": 10}]},
+    {"name": "slime", "current HP": 1, "max HP": 21, "current block": 0, "attack pattern": False, "attack": [{"damage": 4, "block": 0}, {"damage": 6, "block": 6}, {"damage": 5, "block": 0}]},
+    {"name": "fat gremlin", "current HP": 1, "max HP": 32, "current block": 15, "attack pattern": False, "attack": [{"damage": 6, "block": 0}, {"damage": 0, "block": 10}, {"damage": 4, "block": 13}]},
+
+)
+enemies_hard = (
+    {"name": "gremlin nob", "current HP": 1, "max HP": 53, "current block": 0, "attack pattern": True, "attack": [{"damage": 0, "block": 0}, {"damage": 8, "block": 0}, {"damage": 16, "block": 0}, {"damage": 20, "block": 0}, {"damage": 20, "block": 0}, {"damage": 20, "block": 0}]},
+    {"name": "ogre", "current HP": 1, "max HP": 44, "current block": 0, "attack pattern": True, "attack": [{"damage": 15, "block": 0}, {"damage": 15, "block": 0}, {"damage": 0, "block": 5}, {"damage": 15, "block": 0}, {"damage": 15, "block": 0}, {"damage": 0, "block": 5}]},
     )
