4f18aaa0352d74cbaa040410f991485c625da36a
Ethan O'Connor <ethan@northnode.ca>
2024-11-30 19:02:18 -0800

    Write doctests and docstrings for relics.py

--- relics.py
+++ relics.py
@@ -2,39 +2,31 @@ import random
 from text import col
 
 CONST_RELIC_POOL = [
-    {"name": 'strike dummy',
-    "description": 'adds +3 DMG to all strikes',
-    "effect": 3, "one-time": False, "rarity": "uncommon"},
+    {"name": 'strike dummy', "description": 'adds +3 DMG to all strikes', "effect": 3, "one-time": False,
+     "rarity": "uncommon"},
 
-    {"name": 'oddly smooth stone',
-    "description": 'adds +1 to all block',
-    "effect": 1, "one-time": False, "rarity": "common"},
+    {"name": 'oddly smooth stone', "description": 'adds +1 to all block', "effect": 1, "one-time": False,
+     "rarity": "common"},
 
-    {"name": 'regal pillow',
-    "description": 'resting at campfires completely heals you ',
-    "effect": 60, "one-time": False, "rarity": "common"},
+    {"name": 'regal pillow', "description": 'resting at campfires completely heals you ', "effect": 60,
+     "one-time": False, "rarity": "common"},
 
-    {"name": 'blood vial',
-    "description": 'heal 3HP before every battle',
-    "effect": 3, "one-time": False, "rarity": "common"},
+    {"name": 'blood vial', "description": 'heal 3HP before every battle', "effect": 3, "one-time": False,
+     "rarity": "common"},
 
-    {"name": 'old coin',
-    "description": 'immediately gain 200 gold',
-    "effect": 200, "one-time": True, "rarity": "uncommon"},
+    {"name": 'old coin', "description": 'immediately gain 200 gold', "effect": 200, "one-time": True,
+     "rarity": "uncommon"},
 
-    {"name": 'strawberry',
-    "description": 'gain 10 Max HP',
-    "effect": 10, "one-time": True, "rarity": "common"},
+    {"name": 'strawberry', "description": 'gain 10 Max HP', "effect": 10, "one-time": True, "rarity": "common"},
 
-    {"name": 'prepared slug',
-    "description": 'gain 8 block the first turn of combat',
-    "effect": 8, "one-time": False, "rarity": "uncommon"},
-]
+    {"name": 'prepared slug', "description": 'gain 8 block the first turn of combat', "effect": 8, "one-time": False,
+     "rarity": "uncommon"}, ]
 
 CONST_SHOP_RELIC_POOL = [
-    {"name": 'coffee dripper', "description": 'permanently gain +1 Max Energy', "effect": 1, "one-time": True, "rarity": "rare"},
-    {"name": 'philosophers stone', "description": 'permanently gain +1 Max Energy', "effect": 1, "one-time": True, "rarity": "rare"}
-]
+    {"name": 'coffee dripper', "description": 'permanently gain +1 Max Energy', "effect": 1, "one-time": True,
+     "rarity": "rare"},
+    {"name": 'philosophers stone', "description": 'permanently gain +1 Max Energy', "effect": 1, "one-time": True,
+     "rarity": "rare"}]
 
 
 def get_relic():
@@ -43,6 +35,15 @@ def get_relic():
 
     :postcondition: a relic is randomly chosen
     :return: a dictionary of a relic's stats
+
+    >>> relic_list = [{"name": 'prepared slug', "description": 'gain 8 block the first turn of combat', \
+    "effect": 8, "one-time": False, "rarity": "uncommon"}]      # doctest: +SKIP
+    >>> get_relic()         # doctest: +SKIP
+    {"name": 'prepared slug', "description": 'gain 8 block the first turn of combat', \
+    "effect": 8, "one-time": False, "rarity": "uncommon"}
+    >>> relic_list = []  # doctest: +SKIP
+    >>> get_relic()  # doctest: +SKIP
+    congrats, you made me run out of relics
     """
     try:
         return CONST_RELIC_POOL.pop(0)
@@ -78,13 +79,31 @@ def print_shop_relics(shop_list):
     :precondition: shop_list is a non-empty list
     :precondition: shop_list contains a list of dictionaries of relic stats
     :postcondition: the available relics are printed
+    >>> a_shop_list = [({"name": 'regal pillow', "description": "resting at campfires completely heals you", "effect": 60,\
+                       "one-time": False, "rarity": "common"}, 143), \
+                       ({"name": 'oddly smooth stone', "description": 'adds +1 to all block', "effect": 1, \
+                       "one-time": False, "rarity": "common"}, 165)]        # doctest: +SKIP
+    >>> print_shop_relics(a_shop_list)                                      # doctest: +SKIP
+    1) regal pillow - 143 gold
+    - resting at campfires completely heals you
+    <BLANKLINE>
+    2) oddly smooth stone - 165 gold
+    - adds +1 to all block
+    <BLANKLINE>
+     >>> a_shop_list = [({"name": 'regal pillow', "description": "resting at campfires completely heals you", "effect": 60,\
+                       "one-time": False, "rarity": "common"}, 143)]        # doctest: +SKIP
+    >>> print_shop_relics(a_shop_list)                                      # doctest: +SKIP
+    1) regal pillow - 143 gold
+    - resting at campfires completely heals you
+    <BLANKLINE>
     """
     for counter in range(len(shop_list)):
         relic_counter = shop_list[counter]
         relic = relic_counter[0]
         cost = relic_counter[1]
 
-        print(str(counter+1) + ") " + col("!magenta", str(relic['name'])) + " - " + col("yellow", str(cost) + " gold"))
+        print(
+            str(counter + 1) + ") " + col("!magenta", str(relic['name'])) + " - " + col("yellow", str(cost) + " gold"))
         print(col("magenta", "- " + (relic['description'])) + "\n")
 
 
@@ -92,11 +111,32 @@ def shop_relic():
     """
     Generate the shop relics
 
+    Generates three relics plus a shop relic for the player to purchase
     :postcondition: three random relics are generated
     :postcondition: one special relic is chosen
     :postcondition: the relic "old coin" cannot be in the shop
     :postcondition: each relic has a price generated
     :return: a list of all shop relics
+
+    >>> shop_relic()         # doctest: +SKIP
+    [({"name": 'strike dummy', "description": 'adds +3 DMG to all strikes',
+      "effect": 3, "one-time": False, "rarity": "uncommon"}, 175),
+     ({"name": 'oddly smooth stone', "description": 'adds +1 to all block',
+       "effect": 1, "one-time": False, "rarity": "common"}, 165),
+     ({"name": 'regal pillow', "description": 'resting at campfires completely heals you ',
+       "effect": 60,
+       "one-time": False, "rarity": "common"}, 143),
+     ({"name": 'coffee dripper', "description": 'permanently gain +1 Max Energy', "effect": 1,
+       "one-time": True,
+       "rarity": "rare"}, 275)]
+     >>> shop_relic()         # doctest: +SKIP
+    [({"name": 'blood vial', "description": 'heal 3HP before every battle', "effect": 3, "one-time": False,
+     "rarity": "common"}, 151),
+     ({"name": 'strawberry', "description": 'gain 10 Max HP', "effect": 10, "one-time": True, "rarity": "common"}, 145),
+     ({"name": 'oddly smooth stone', "description": 'adds +1 to all block',
+       "effect": 1, "one-time": False, "rarity": "common"}, 149),
+     ({"name": 'philosophers stone', "description": 'permanently gain +1 Max Energy', "effect": 1, "one-time": True,
+     "rarity": "rare"}, 275)]
     """
     print("")
     gold_price = []
@@ -119,7 +159,7 @@ def shop_relic():
         shop_list.append(relic_dict)
     shop_list.append((random.choice(CONST_SHOP_RELIC_POOL), 275))
 
-    #print_shop_relics(shop_list)
+    # print_shop_relics(shop_list)
     return shop_list
 
 
@@ -131,6 +171,18 @@ def print_relic_description(relic):
     :precondition: relic is a dictionary of a relic's stats
     :postcondition: the relic description is printed
     :postcondition: relics with the effect "one-time" have their effect printed
+
+    >>> a_relic = {"name": 'old coin', "description": 'immediately gain 300 gold', "effect": 300, \
+    "one-time": True, "rarity": "uncommon"}     # doctest: +SKIP
+    >>> print_relic_description(a_relic)        # doctest: +SKIP
+    old coin 300
+    - immediately gain 300 gold
+
+    >>> a_relic = {"name": 'strike dummy', "description": 'adds +3 DMG to all strikes', \
+                      "effect": 3, "one-time": False, "rarity": "uncommon"}     # doctest: +SKIP
+    >>> print_relic_description(a_relic)                                        # doctest: +SKIP
+    strike dummy
+    - adds +3 DMG to all strikes
     """
     if relic["one-time"]:
         print(col("!magenta", str(relic['name'])) + " " + col("!black", str(relic['effect'])))
@@ -159,7 +211,7 @@ def relic_one_time_buff(relic, player):
     """
     if relic['one-time']:
         if relic['name'] == "old coin":
-            player["Gold"] += 200
+            player["Gold"] += 300
             relic["effect"] = "This relic has been used up"
 
         elif relic['name'] == "strawberry":
@@ -195,7 +247,7 @@ def purchase_relic(relic, player):
     if player["Gold"] >= cost:
         player["Gold"] -= cost
         player["Relics"].append(relic[0])
-        relic_one_time_buff(relic[0], player)           # check if relic is one time use after purchase
+        relic_one_time_buff(relic[0], player)  # check if relic is one time use after purchase
         return True
     else:
         return False
